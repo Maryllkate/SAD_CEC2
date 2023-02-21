0f80850c7d23ed4813aa63f8912bb09a5a358c8b
maryll kate <maryll@prosperna.com>
2023-02-21 19:50:43 +0800

    adding bootstrap files

--- include/home/login.php
+++ include/home/login.php
@@ -1,8 +1,40 @@
-    <div class="container">
-        <div class="left-container">
-      
-        </div>
-        <div class="right-container">
-            right
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>login</title>
+
+</head>
+<body>
+    <div class="container"> 
+        <form class="col-left">
+            
+        </form>
+        <form class="col-right">
+            <section class="right-banner">
+            <h3>WELCOME</h3>
+            </section>
+            <form class="form">
+                <section class="login-section">
+                    <div class="username">
+                    <label for="username">Username</label> <br/>
+                    <input type="text" class="uname" placeholder="Enter Username" required> <br/>
+                    </div>
+                    <div class="password">
+                    <label for="password">Password</label> <br/>
+                    <input type="text" class="psw" placeholder="Enter Password" required> <br/>
+                    </div>
+                    <button type="login" class="login">login</button>
+                </section>
+            </form>
+        </form>
     </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,2 @@
-<?php include("include/home/header.php"); ?>
-
 <?php include("include/home/login.php"); ?>
-
-<?php include("include/home/footer.php"); ?>
 
