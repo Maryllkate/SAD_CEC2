7b6ef6253befa2767ea0fc5a4910f1ae02a7142c
maryll kate <maryll@prosperna.com>
2023-02-21 14:58:07 +0800

    first commit

---
+++ conn/conn.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "system";
+
+    $conn = mysqli_connect($servername, 
+                           $username,
+                           $password,
+                           $database);
+    
+    //Checking connection
+    if (!$conn) {
+        die ("Connection failed : " . mysqli_connect_error());
+    }
+    echo "Connection success";
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include("include/home/header.php"); ?>
+
+<?php include("include/home/main.php"); ?>
+
+<?php include("include/home/footer.php"); ?>
+
