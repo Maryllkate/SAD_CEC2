8dec80e2e3cf694fc9fa1bff73b151fa74a52f5c
maryll kate <maryll@prosperna.com>
2023-04-19 09:55:33 +0800

    added popper script in header & change dropdown into BS

--- accessability.php
+++ accessability.php
@@ -11,19 +11,30 @@
                     <label for="new-password"><span>Create Password</span></label>
                     <input type="password" class="ant-input text-input error" placeholder="Enter new Password" required>
 
-                    <label for="usertype"><span>Select Usertype: </span></label>
+                    <!-- <label for="select-usertype" class="select-usertype">Select a usertype</label> -->
                     <form action="">
-                        <select name="usertypes" id="usertypes">
-                            <option value="Administrator">Administrator</option>
+                        <!-- <select name="usertypes" id="usertypes"> -->
+                            <div class="dropdown">
+                                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Select Usertype:
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a href="#" class="dropdown-item">Administrator</a>
+                                    <a href="#" class="dropdown-item">Manager</a>
+                                    <a href="#" class="dropdown-item">Supervisor</a>
+                                    <a href="#" class="dropdown-item">Staff</a>
+                                </div>
+                            </div>
+                            <!-- <option value="Administrator">Administrator</option>
                             <option value="Administrator">Manager</option>
                             <option value="Administrator">Supervisor</option>
                             <option value="Administrator">Staff</option>
-                        </select>
-                    </form>
-
-                    <button type="register" class="btn btn-primary btn-register">
+                         </select> -->
+                        <button type="register" class="btn btn-primary btn-register">
                         <span>Register</span>
-                    </button>
+                        </button>
+
+                    </form>
                 </form>
             </div>
         </div>
