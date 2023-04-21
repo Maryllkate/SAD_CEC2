5c8288e7d0d569ba741ef8c1cb1588df2a6b79b8
Maryllkate <maryllkate20@gmail.com>
2023-04-21 12:14:57 +0800

    rename product location to product Model

--- sidebar.php
+++ sidebar.php
@@ -34,7 +34,7 @@
                         <li>
                             <a href="./product_location.php">
                                 <span class="icon"><i class="bi bi-geo-fill"></i></span>
-                                <span class="item">Product Location</span>
+                                <span class="item">Product Model</span>
                             </a>
                         </li>
                         <li>
