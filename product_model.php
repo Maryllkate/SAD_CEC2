3dfcda69395b33c3fc35f880e19ce2b8fc128b59
Maryllkate <maryllkate20@gmail.com>
2023-04-22 11:46:11 +0800

    added BS buttons for searchbar

--- product_model.php
+++ product_model.php
@@ -4,13 +4,30 @@
     <div class="product_location-left">
         <div class="container-fluid con-product-model">
             <div class="categories">
-                dropdown selections from categories
+                <div class="input-group">
+                    <div class="selection-categories input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">Select Categories</label>
+                        <select id="inputGroupSelect01" class="custom-select">
+                            <option selected>Choose...</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
             </div>
+            
             <div class="sorting">
                 sorting by of products
-                <div class="searchbar">
-                    searchbar floating in right
-                </div>
+                
+                <!-- <div class="searchbar"> -->
+                    <div class="input-group-append grp-searchbar">
+                        <input type="search" class="form-control-search" placeholder="Search Product Model" aria-label="searchProductModel" aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary btn-search" type="button">Search</button>
+                        </div>
+                    </div>
+                <!-- </div> -->
             </div>
             <div class="product-images inventory-images">
                     images from inventory
