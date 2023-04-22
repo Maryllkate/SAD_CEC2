8596f8ac80451663440f2400453207f9de2fe8ed
Maryllkate <maryllkate20@gmail.com>
2023-04-22 13:42:12 +0800

    added navbar element

--- product_model.php
+++ product_model.php
@@ -2,37 +2,46 @@
 
 
     <div class="product_location-left">
-        <div class="container-fluid con-product-model">
-            <div class="categories">
-                <div class="input-group">
-                    <div class="selection-categories input-group-prepend">
-                        <label class="input-group-text" for="inputGroupSelect01">Select Categories</label>
-                        <select id="inputGroupSelect01" class="custom-select">
-                            <option selected>Choose...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+        <nav class="navbar bg-light">
+            <div class="container-fluid con-product-model">
+
+                <div class="input-group-append grp-searchbar">
+                    <input type="search" class="form-control-search" 
+                                         placeholder="Search Product Model" 
+                                         aria-label="searchProductModel" 
+                                         aria-describedby="basic-addon2"
+                                         />
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-primary btn-search" type="button">Search</button>
                     </div>
-                </div>
-            </div>
-            
-            <div class="sorting">
-                sorting by of products
-                
-                <!-- <div class="searchbar"> -->
-                    <div class="input-group-append grp-searchbar">
-                        <input type="search" class="form-control-search" placeholder="Search Product Model" aria-label="searchProductModel" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary btn-search" type="button">Search</button>
+
+                    <div class="input-group">
+                        <div class="selection-categories input-group-prepend">
+                            <label class="input-group-text" for="inputGroupSelect01">Select Categories</label>
+                                <select id="inputGroupSelect01" class="custom-select">
+                                    <option selected>Choose...</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                            </select>
                         </div>
                     </div>
-                <!-- </div> -->
+        
+                </div>
+       
+        </nav>
+
+                    <div class="product-images inventory-images">
+                        <div class="forms-of-products">
+                            forms of products
+                        </div>
+                    </div>
+
+
+
+
+
             </div>
-            <div class="product-images inventory-images">
-                    images from inventory
-            </div>
-        </div>
     </div>
 
 </div>
