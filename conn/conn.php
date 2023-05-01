0cada6453bdf885c09c86232b3f4c4f86da40735
Mulan <maryllkate20@gmail.com>
2023-05-01 10:56:10 +0800

    Update conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -4,7 +4,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "inventory_databasec: ";
+    $database = "inventory_database ";
 
     $conn = mysqli_connect($servername, 
                            $username,
